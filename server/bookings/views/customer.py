42864ac08ce4e88f3ccb1d4e86ecdf30082ed161
Eba Teshale <ebateshale40@gmail.com>
2026-07-30 03:08:58 +0300

    adding the actions of status by customer or providers

--- server/bookings/views/customer.py
+++ server/bookings/views/customer.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from bookings.models import Booking
 from bookings.serializers import BookingSerializer
 
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class CustomerBookingViewSet(ModelViewSet):
     serializer_class = BookingSerializer
@@ -18,6 +20,81 @@ class CustomerBookingViewSet(ModelViewSet):
                 "provider__user",
                 "service",
             )
+        )
+      # Cancel booking
+    @action(detail=True, methods=["patch"])
+    def accept(self, request, px=None):
+        booking = self.get_object()
+        allowed_status = [
+            "pending",
+            "accepted"
+        ]
+
+        if booking.status not in allowed_status:
+            return Response(
+                {
+                    "error":
+                    "This booking cannot be cancelled"
+                },
+                status=400
+            )
+        booking.status = "cancelled"
+        booking.save()
+        return Response(
+            {
+                    "message":
+                    "Booking cancelled successfully",
+
+                    "status":
+                    booking.status
+            },
+        )
+
+    # Upcoming bookings
+
+    @action(detail=False,methods=["get"])
+    def upcoming(self, request):
+
+        bookings = self.get_queryset().filter(
+            status__in=[
+                "pending",
+                "accepted",
+                "started"
+            ]
+        )
+
+
+        serializer = self.get_serializer(
+            bookings,
+            many=True
+        )
+
+
+        return Response(
+            serializer.data
+        )
+
+    # Completed history
+
+    @action(
+        detail=False,
+        methods=["get"]
+    )
+    def history(self, request):
+
+        bookings = self.get_queryset().filter(
+            status="completed"
+        )
+
+
+        serializer = self.get_serializer(
+            bookings,
+            many=True
+        )
+
+
+        return Response(
+            serializer.data
         )
 
     def perform_create(self, serializer):
--- server/bookings/views/provider.py
+++ server/bookings/views/provider.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from bookings.models import Booking
 from bookings.serializers import BookingSerializer
@@ -19,4 +21,97 @@ class ProviderBookingViewSet(ModelViewSet):
                 "provider__user",
                 "service",
             )
+        )
+
+    @action(detail=True, methods=["patch"])
+    def accept(self, request, px=None):
+        booking = self.get_object()
+        if booking.status != "pending":
+            return Response(
+                {
+                    "error":
+                    "only pending bookings can be accepted"
+                },
+                status=400
+            )
+        booking.status = "accepted"
+        booking.save()
+        return Response(
+            {
+                 "message":
+                 "Booking accepted accepted",
+
+                 "status":
+                 booking.status
+            },
+        )
+
+    @action(detail=True, methods=["patch"])
+    def accept(self, request, px=None):
+        booking =self.get_object()
+
+        if booking.status != "pending":
+            return Response(
+                {
+                    "error":
+                    "only pending"
+                },
+                status=400
+            )
+        booking.status = "rejected"
+        booking.save()
+        return Response(
+            {
+                "message":
+                "Booking Rejected",
+
+                "status":
+                booking.status
+            }
+        )
+    @action(detail=True, methods=["patch"])
+    def accept(self, request, px=None):
+        booking =self.get_object()
+
+        if booking.status != "pending":
+            return Response(
+                {
+                    "error":
+                    "only pending"
+                },
+                status=400
+            )
+        booking.status = "started"
+        booking.save()
+        return Response(
+            {
+                "message":
+                "Job started",
+
+                "status":
+                booking.status
+            }
+        )
+    @action(detail=True, methods=["patch"])
+    def accept(self, request, px=None):
+        booking =self.get_object()
+
+        if booking.status != "pending":
+            return Response(
+                {
+                    "error":
+                    "only pending"
+                },
+                status=400
+            )
+        booking.status = "completed"
+        booking.save()
+        return Response(
+            {
+                "message":
+                "Job completed",
+
+                "status":
+                booking.status
+            }
         )
