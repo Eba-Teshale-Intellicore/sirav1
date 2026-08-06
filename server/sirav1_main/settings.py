bc3e048be559e1674633793c6d6bfa6f0f47dcd4
Eba Teshale <ebateshale40@gmail.com>
2026-08-07 00:23:46 +0300

    kejf

--- server/sirav1_main/settings.py
+++ server/sirav1_main/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h7t80i@vqctnf_r-!vrna-9c*_l93fkaw7qz(jl2cf8&l0x56x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG =DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 # ALLOWED_HOSTS = []
