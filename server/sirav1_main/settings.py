1519f703fd8ccd35563657eb0959ef89617987ea
Eba Teshale <ebateshale40@gmail.com>
2026-08-09 20:30:35 +0300

    django ins

--- server/sirav1_main/settings.py
+++ server/sirav1_main/settings.py
@@ -26,6 +26,7 @@ import dj_database_url
 import os
 from datetime import timedelta
 
+GOOGLE_WEB_CLIENT_ID = os.environ.get("GOOGLE_WEB_CLIENT_ID")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
