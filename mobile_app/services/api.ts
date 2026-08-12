71a98f2ef5c8bfdca667d6e1295ffb39d12ea2bf
Eba Teshale <ebateshale40@gmail.com>
2026-08-12 17:59:49 +0300

    fix some error

--- mobile_app/services/api.ts
+++ mobile_app/services/api.ts
@@ -7,9 +7,33 @@
 //   //   "Content-Type": "application/json",
 //   // },
 // });
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "https://sirav1-1.onrender.com",
+//   timeout: 15000,
+// });
 import axios from "axios";
+import { getAccessToken } from "./authStorage";
 
 export const api = axios.create({
   baseURL: "https://sirav1-1.onrender.com",
-  timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await getAccessToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
